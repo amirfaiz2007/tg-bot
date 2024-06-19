a707545d18458d96b6583e8e2c711d60b0ade1da
amirfaiz <amirfaiz2007@gmail.com>
2024-06-19 14:45:04 +0300

    изменен текст сообщения старт

--- handlers/start.py
+++ handlers/start.py
@@ -12,7 +12,7 @@ router = Router()
 @router.message(CommandStart())
 async def start(message: Message, state: FSMContext):
     await state.set_state(Form.wait)
-    text = "Привет! Перед началом необходимо заполнить небольшую анкету. Нажми на кнопку ниже, чтобы перейти к заполению"
+    text = "Привет! Перед началом необходимо заполнить небольшую анкету. Нажми на кнопку ниже, чтобы перейти к заполению!"
     await message.answer(text=text, reply_markup=form_button)
     photo = await get_photo('animal')
     await message.answer_photo(photo=photo)
