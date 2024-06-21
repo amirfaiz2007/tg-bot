e8cdaa58d885fb8b858185b365e16a92688710fc
amirfaiz <amirfaiz2007@gmail.com>
2024-06-21 10:28:24 +0300

    доработка картинок

--- erg.py
+++ erg.py
@@ -1,6 +1,6 @@
 import sqlite3 as sq
 
-db = sq.connect("C:/Users/ARTUR/PycharmProjects/tg bot/data base.db")
+db = sq.connect("data base.db")
 cur = db.cursor()
 async def get_photo(name):
     return cur.execute(f"SELECT id FROM images where name == '{name}'").fetchone()[0]
